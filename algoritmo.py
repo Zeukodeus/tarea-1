f323437d45ba2466d066e4d787eb6fb02bc74002
unknown <carlos2234503@correo.uis.edu.co>
2023-03-03 22:41:44 -0500

    tarea-1

---
+++ algoritmo.py
@@ -0,0 +1,9 @@
+J = int(input("Digite un número: "))
+K = int(input("Digite otro número: "))
+
+if J>K :
+    rta = str(J) + " Es mayor que " + str(K)
+else :
+    rta = str(J) + " Es menor que " + str(K)
+print (str(rta))
+     
